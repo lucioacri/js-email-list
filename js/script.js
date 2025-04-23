d6740166f7634707653cd6da9640539e487f0572
lucioacri <lucioacri@gmail.com>
2025-04-23 16:04:22 +0200

    deleted array

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-const emailsArray = [];
 const generateButton = document.getElementById("generate-button");
 const emailsForm = document.getElementById("emails-form");
 const emailsList = document.getElementById("emails-list");
@@ -8,17 +7,13 @@ const getRandomEmail = () => {
     .get("https://flynn.boolean.careers/exercises/api/random/mail")
     .then((response) => {
       const email = response.data.response;
-      emailsArray.push(email);
-      let htmlmails = ``;
-      for (const mail of emailsArray) {
-        htmlmails += `<li class="list-group-item">${mail}</li>`;
-      }
-      emailsList.innerHTML = htmlmails;
+      emailsList.innerHTML += `
+        <li>${email}</li>
+      `;
     });
 };
 
 const generateEmail = (number) => {
-  emailsArray.length = 0;
   emailsList.innerHTML = ``;
   for (let i = 0; i < number; i++) {
     getRandomEmail();
@@ -29,5 +24,4 @@ const generateEmail = (number) => {
 emailsForm.addEventListener("submit", function (e) {
   e.preventDefault();
   generateEmail(10);
-  console.log(emailsArray);
 });
