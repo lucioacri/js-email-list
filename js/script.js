048a5115aff660a0af996ae4f850825419bdaedb
lucioacri <lucioacri@gmail.com>
2025-04-23 14:31:31 +0200

    add second function+starting html

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 const emailsArray = [];
+const generateButton = document.getElementById("generate-button");
+const emailsForm = document.getElementById("emails-form");
+
 const getRandomEmail = () => {
   axios
     .get("https://flynn.boolean.careers/exercises/api/random/mail")
@@ -8,7 +11,11 @@ const getRandomEmail = () => {
     });
 };
 
-for (let i = 0; i < 10; i++) {
-  getRandomEmail();
-}
+const generateEmail = (number) => {
+  emailsArray.length = 0;
+  for (let i = 0; i < number; i++) {
+    getRandomEmail();
+  }
+};
+generateEmail(10);
 console.log(emailsArray);
