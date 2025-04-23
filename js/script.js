38dc50a21f46c15e96e94d9bdf8b254e75c3bd44
lucioacri <lucioacri@gmail.com>
2025-04-23 15:10:36 +0200

    add generator button

--- js/script.js
+++ js/script.js
@@ -1,22 +1,31 @@
 const emailsArray = [];
 const generateButton = document.getElementById("generate-button");
 const emailsForm = document.getElementById("emails-form");
-
+const emailsList = document.getElementById("emails-list");
+// functions
 const getRandomEmail = () => {
   axios
     .get("https://flynn.boolean.careers/exercises/api/random/mail")
     .then((response) => {
       const email = response.data.response;
       emailsArray.push(email);
+      let htmlmails = ``;
+      for (const mail of emailsArray) {
+        htmlmails += `<li class="list-group-item">${mail}</li>`;
+      }
+      emailsList.innerHTML = htmlmails;
     });
 };
 
 const generateEmail = (number) => {
   emailsArray.length = 0;
+  emailsList.innerHTML = ``;
   for (let i = 0; i < number; i++) {
     getRandomEmail();
   }
 };
+
+// html update
 emailsForm.addEventListener("submit", function (e) {
   e.preventDefault();
   generateEmail(10);
